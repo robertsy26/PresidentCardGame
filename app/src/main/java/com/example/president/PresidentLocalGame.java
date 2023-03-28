a73befe7a0961ec25f2a6fcffe151ed71cffb099
AnishKing2004 <67561021+AnishKing2004@users.noreply.github.com>
2023-03-27 18:10:43 -0700

    - Finished Alternate Players Method - Finished checkIfGameOver method - Finished canMove method

--- app/src/main/java/com/example/president/PresidentLocalGame.java
+++ app/src/main/java/com/example/president/PresidentLocalGame.java
@@ -4,6 +4,8 @@ import com.example.GameFramework.LocalGame;
 import com.example.GameFramework.actionMessage.GameAction;
 import com.example.GameFramework.players.GamePlayer;
 
+import java.util.ArrayList;
+
 public class PresidentLocalGame extends LocalGame {
     PresidentGameState president;
     @Override
@@ -13,13 +15,31 @@ public class PresidentLocalGame extends LocalGame {
 
     @Override
     protected boolean canMove(int playerIdx) {
+        if(president.getPlayerId() == playerIdx){
+            return true;
+        }
         return false;
     }
 
     @Override
     protected String checkIfGameOver() {
+        ArrayList<Integer> checkPlayer1 = president.getPlayer1Cards();
+        ArrayList<Integer> checkPlayer2 = president.getPlayer2Cards();
+        ArrayList<Integer> checkPlayer3 = president.getPlayer3Cards();
+        ArrayList<Integer> checkPlayer4 = president.getPlayer4Cards();
 
-
+        if(checkPlayer1.isEmpty()){
+            return "Player 1 winss";
+        }
+        if(checkPlayer2.isEmpty()){
+            return "Player 2 winss";
+        }
+        if(checkPlayer3.isEmpty()){
+            return "Player 3 winss";
+        }
+        if(checkPlayer4.isEmpty()){
+            return "Player 4 winss";
+        }
         return null;
     }
 
