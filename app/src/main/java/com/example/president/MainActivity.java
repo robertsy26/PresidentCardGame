593a9aecbc90b0f89a8df457d8f5482bea41e180
AnishKing2004 <67561021+AnishKing2004@users.noreply.github.com>
2023-03-30 10:04:16 -0700

    - App no crash anymore - shuffle button is back - Ui looking pretty cool

--- app/src/main/java/com/example/president/MainActivity.java
+++ app/src/main/java/com/example/president/MainActivity.java
@@ -11,28 +11,146 @@ import android.widget.ImageView;
 import com.example.GameFramework.GameMainActivity;
 import com.example.GameFramework.LocalGame;
 import com.example.GameFramework.gameConfiguration.GameConfig;
+import com.example.GameFramework.gameConfiguration.GamePlayerType;
 import com.example.GameFramework.infoMessage.GameState;
+import com.example.GameFramework.players.GamePlayer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class MainActivity extends GameMainActivity {
+public class MainActivity extends AppCompatActivity {
+    ImageView iv_deck, iv_card1,iv_card2,iv_card3,iv_card4,iv_card5,iv_card6,iv_card7,iv_card8,iv_card9,iv_card10,iv_card11,iv_card12,iv_card13;
+    Cards card = new Cards();
 
-    private static final int PORT_NUMBER = 3384;
-
-
-    @Override
-    public GameConfig createDefaultConfig() {
-        return null;
-    }
 
     @Override
-    public LocalGame createLocalGame(GameState gameState) {
-        return null;
+    protected void onCreate (Bundle savedInstanceSate) {
+        super.onCreate(savedInstanceSate);
+        setContentView(R.layout.activity_main);
+
+        iv_deck = (ImageView) findViewById(R.id.iv_deck);
+        iv_card1 = (ImageView) findViewById(R.id.iv_card1);
+        iv_card2 = (ImageView) findViewById(R.id.iv_card2);
+        iv_card3 = (ImageView) findViewById(R.id.iv_card3);
+        iv_card4 = (ImageView) findViewById(R.id.iv_card4);
+        iv_card5 = (ImageView) findViewById(R.id.iv_card5);
+        iv_card6 = (ImageView) findViewById(R.id.iv_card6);
+        iv_card7 = (ImageView) findViewById(R.id.iv_card7);
+        iv_card8 = (ImageView) findViewById(R.id.iv_card8);
+        iv_card9 = (ImageView) findViewById(R.id.iv_card9);
+        iv_card10 = (ImageView) findViewById(R.id.iv_card10);
+        iv_card11 = (ImageView) findViewById(R.id.iv_card11);
+        iv_card12 = (ImageView) findViewById(R.id.iv_card12);
+        iv_card13 = (ImageView) findViewById(R.id.iv_card13);
+
+        iv_card1.setVisibility(View.INVISIBLE);
+        iv_card2.setVisibility(View.INVISIBLE);
+        iv_card3.setVisibility(View.INVISIBLE);
+        iv_card4.setVisibility(View.INVISIBLE);
+        iv_card5.setVisibility(View.INVISIBLE);
+        iv_card6.setVisibility(View.INVISIBLE);
+        iv_card7.setVisibility(View.INVISIBLE);
+        iv_card8.setVisibility(View.INVISIBLE);
+        iv_card9.setVisibility(View.INVISIBLE);
+        iv_card10.setVisibility(View.INVISIBLE);
+        iv_card11.setVisibility(View.INVISIBLE);
+        iv_card12.setVisibility(View.INVISIBLE);
+        iv_card13.setVisibility(View.INVISIBLE);
+
+        iv_deck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                card.setCards();
+                Collections.shuffle(card.cards);
+
+                card.assignImages(card.cards.get(0), iv_card1);
+                card.assignImages(card.cards.get(1), iv_card2);
+                card.assignImages(card.cards.get(2), iv_card3);
+                card.assignImages(card.cards.get(3), iv_card4);
+                card.assignImages(card.cards.get(4), iv_card5);
+                card.assignImages(card.cards.get(5), iv_card6);
+                card.assignImages(card.cards.get(6), iv_card7);
+                card.assignImages(card.cards.get(7), iv_card8);
+                card.assignImages(card.cards.get(8), iv_card9);
+                card.assignImages(card.cards.get(9), iv_card10);
+                card.assignImages(card.cards.get(10), iv_card11);
+                card.assignImages(card.cards.get(11), iv_card12);
+                card.assignImages(card.cards.get(12), iv_card13);
+
+                iv_card1.setVisibility(View.VISIBLE);
+                iv_card2.setVisibility(View.VISIBLE);
+                iv_card3.setVisibility(View.VISIBLE);
+                iv_card4.setVisibility(View.VISIBLE);
+                iv_card5.setVisibility(View.VISIBLE);
+                iv_card6.setVisibility(View.VISIBLE);
+                iv_card7.setVisibility(View.VISIBLE);
+                iv_card8.setVisibility(View.VISIBLE);
+                iv_card9.setVisibility(View.VISIBLE);
+                iv_card10.setVisibility(View.VISIBLE);
+                iv_card11.setVisibility(View.VISIBLE);
+                iv_card12.setVisibility(View.VISIBLE);
+                iv_card13.setVisibility(View.VISIBLE);
+
+                iv_deck.setVisibility((View.INVISIBLE));
+                iv_deck.setClickable(false);
+
+
+            }
+        });
     }
+
+
+
+
 
 
 
 
 
 }
+
+
+
+
+//private static final int PORT_NUMBER = 3384;
+
+
+
+
+   /* @Override
+    public GameConfig createDefaultConfig() {
+
+        ArrayList<GamePlayerType> playerType = new ArrayList<>();
+        playerType.add(new GamePlayerType("Human Player") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new HumanPlayer(name);
+            }
+        });
+
+        playerType.add(new GamePlayerType("Computer Player") {
+            @Override
+            public GamePlayer createPlayer(String name) {
+                return new PresidentComputerPlayer(name);
+            }
+        });
+
+        GameConfig defaultConfig = new GameConfig(playerType, 4, 4, "Pig", PORT_NUMBER);
+        defaultConfig.addPlayer("Human", 0); // player 1: a human player
+        defaultConfig.addPlayer("Computer", 1); // player 2: a computer player
+        defaultConfig.addPlayer("Computer", 2);
+        defaultConfig.addPlayer("Computer",4);
+
+        defaultConfig.setRemoteData("Remote Human Player", "", 0);
+
+        return defaultConfig;
+
+
+    }
+
+
+    public LocalGame createLocalGame(GameState gameState) {
+        return new PresidentLocalGame();
+    }
+*/
